bf5447ee7afcbfec77f3915fd8c63f5a69fc56dc
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-07-30 13:46:33 +0800

    App skeleton

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'mysql2', '~> 0.2.6'
+gem 'jquery-rails'
+gem 'rails3-generators'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development, :test do
+	gem 'rspec-rails'
+  gem 'shoulda-matchers'
+	gem 'capybara'
+	gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackweekend::Application.config.secret_token = 'd7ee86ba9c94842f51c6adc38329505c3049fba36b646ffe6d087f151160bf0af8b99e1f5abc367ea2ae21e5936f07bb241c862d84be7881b185c02f8bb3f6a2'
