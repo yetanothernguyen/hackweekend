a3e3d1f27be6f156c9ecd80f9af3fed9f4c4ac18
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-07-30 16:14:57 +0800

    Add Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Hackweekend::Application.routes.draw do
+  root :to => "pages#home"
+  get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
