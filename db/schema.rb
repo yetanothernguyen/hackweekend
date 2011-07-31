1bf41c11903ccc15851bba53cb59946ee9b4357e
Gaddafi <flisterz@gmail.com>
2011-07-31 09:12:26 +0800

    bounty style

--- db/schema.rb
+++ db/schema.rb
@@ -35,15 +35,6 @@ ActiveRecord::Schema.define(:version => 20110731005207) do
     t.datetime "updated_at"
   end
 
-  create_table "contacts", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "phone"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "first_quests", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
